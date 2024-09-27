59e16f976cfa461ab6ae0cbb1f6ed4354575a6c7
YoussefYasser <@gmail>
2024-09-27 23:22:46 +0300

    Add first file

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myWidgit());
+}
+
+class myWidgit extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          floatingActionButton: FloatingActionButton(onPressed:(){}),
+          appBar: AppBar(
+            title: Text("title for app bar kkkkk"),
+            backgroundColor: Colors.red,
+            foregroundColor:Colors.deepPurpleAccent,
+          ),
+          body: Center(
+            child: Text('this is my word in new'),
+          ),
+        ));
+  }
+}
